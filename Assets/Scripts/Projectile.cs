995460da197f720fc1efa43cb4777a9b97a45441
Liliane Castro <lcs2@icomp.ufam.edu.br>
2021-01-06 14:45:35 -0400

    feat: projectile

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Projectile : MonoBehaviour
 { 
     [SerializeField]
     [Range(1, 20)] private float speed = 4f;
 
+    [SerializeField]
+    [Range(1, 4)] private float timeToDestroy = 2f;
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(timeToDestroy);
+
+        Destroy(this.gameObject);
+    }
+    
     private void Update()
     {
         transform.Translate(Vector3.up * speed * Time.deltaTime);
