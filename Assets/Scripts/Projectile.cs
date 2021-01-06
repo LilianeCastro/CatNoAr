bb10d7624613522b97f1fd0f3b15f1ba287cdcb1
Liliane Castro <lcs2@icomp.ufam.edu.br>
2021-01-05 20:00:06 -0400

    feat: trail

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{ 
+    [SerializeField]
+    [Range(1, 20)] private float speed = 4f;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -7,7 +7,6 @@ public class Shooter : MonoBehaviour
 {
     private Control _control;
     private Camera _camera;
-    private LineRenderer _lineRenderer;
 
     private bool _canShoot = true;
 
@@ -25,8 +24,6 @@ public class Shooter : MonoBehaviour
     {
         _control = new Control();
         _camera = Camera.main;
-        
-        _lineRenderer = GetComponent<LineRenderer>();
     }
 
     private void OnEnable()
@@ -55,8 +52,6 @@ public class Shooter : MonoBehaviour
         if ( angle > 0 && angle < 190)
         {
             transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-            _lineRenderer.SetPosition(0, new Vector3(_directionToShoot.position.x, _directionToShoot.position.y, _directionToShoot.position.z));
-            _lineRenderer.SetPosition(1, transform.position);
         }
             
     }
@@ -64,11 +59,12 @@ public class Shooter : MonoBehaviour
     private void Shoot()
     {
         if (!_canShoot) { return ;}
-        _canShoot = false;
-        Instantiate(_projectile, _directionToShoot.position, _directionToShoot.rotation);
 
+        _canShoot = false;
+        
+        Instantiate(_projectile, _directionToShoot.position, _directionToShoot.rotation);
+        
         StartCoroutine(DelayShoot());
-        //se tiver carregado pode atirar
     }
 
     private IEnumerator DelayShoot()
